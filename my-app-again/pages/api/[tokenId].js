1efd83e3783a25954ab98d7743df17f3e0b68b12
AdarshGoel2001 <86413540+AdarshGoel2001@users.noreply.github.com>
2022-07-30 06:32:10 +0530

    fir

--- my-app-again/pages/api/[tokenId].js
+++ my-app-again/pages/api/[tokenId].js
@@ -4,5 +4,9 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   const name = `Crypto Dev # ${tokenId}`;
   const description = "CryptoDevs is an NFT colection for Web3 Developers";
-  const image = "";
+  const image = `https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/${
+    Number(tokenId) - 1
+  }.svg`;
+
+  return res.json({ name: name, description: description, image: image });
 }
