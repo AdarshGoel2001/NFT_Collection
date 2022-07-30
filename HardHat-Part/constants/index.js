933faebda7584d147785bc11fdbffe35318c1e42
AdarshGoel2001 <86413540+AdarshGoel2001@users.noreply.github.com>
2022-07-30 06:16:47 +0530

    first push

---
+++ HardHat-Part/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0xd3510bde2aDed913ac9DE5d3B46057A72cC4695A";
+
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
